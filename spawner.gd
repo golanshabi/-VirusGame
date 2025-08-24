7fce7fbad2106113b55cf776aa589e2190c271b3
golan shabi <golan.shabi@gmail.com>
2025-08-24 21:16:56 +0300

    Added stuff to spawner and finished level three

--- spawner.gd
+++ spawner.gd
@@ -1,12 +1,12 @@
 extends Node2D
 var bullet = preload("res://Turret/bullet.tscn")
 @onready var cooldown_timer = $"../CoolDownTimer"
-const TIME_PER_SHOT = 1
 var stop = false
 
 func _process(delta: float) -> void:
 	if cooldown_timer.is_stopped() && not stop:
-		cooldown_timer.wait_time = TIME_PER_SHOT
+		cooldown_timer.wait_time = get_parent().TIME_PER_SHOT
+		print(get_parent().TIME_PER_SHOT)
 		cooldown_timer.start()
 		var bullet_instance = bullet.instantiate()
 		get_parent().add_child(bullet_instance)
