86d2c9ef6a54cf487b8431a8ed029b6b6c364115
dantal1602 <danieltal1602@gmail.com>
2025-08-24 20:14:04 +0300

    coyote jump

--- Player/player.gd
+++ Player/player.gd
@@ -25,6 +25,8 @@ var hp_gain_from_coins = 10
 @onready var Dash_cd_bar = get_node("/root/main_scene/canvas/HUD/Dash_cd")
 @onready var Clone_cd_bar = get_node("/root/main_scene/canvas/HUD/Clone_cd")
 @onready var finish_level_mask = $Finish_level_mask
+@onready var able_to_jump_timer = $AbleToJumpTimer
+var jump_ledge_timer = 0.3
 
 var enemy_hit_sound_array = []
 var player_hit_sound_array = []
@@ -41,6 +43,8 @@ var dead : bool = false
 var last_direction = 1
 var jump_state : JumpState = JumpState.FLOOR
 
+var grounded : bool = false
+
 enum JumpState {
 	TRANS_JUMP,
 	JUMP,
@@ -53,6 +57,7 @@ func reset_level_mask():
 	finish_level_mask.scale = Vector2(2,2)
 
 func _ready():
+	able_to_jump_timer.wait_time = jump_ledge_timer
 	reset_level_mask()
 	player_hit_sound_array.append(preload("res://Assetes/Music/hit_effect_1.wav"))
 	player_hit_sound_array.append(preload("res://Assetes/Music/hit_effect_2.wav"))
@@ -92,8 +97,16 @@ func _physics_process(delta: float) -> void:
 		if velocity.y < 0 and Input.is_action_pressed("ui_accept"):
 			gravity = gravity * GRAVITY_REDUCTION_RATIO
 		velocity += gravity
-
-	if Input.is_action_just_pressed("jump") and is_on_floor() and is_not_input_restricted():
+		
+	if is_on_floor():
+		grounded = true
+	else:
+		if grounded:
+			able_to_jump_timer.wait_time = jump_ledge_timer
+			able_to_jump_timer.start()
+		grounded = false
+		
+	if Input.is_action_just_pressed("jump") and can_jump() and is_not_input_restricted():
 		velocity.y = JUMP_SINGLE_VELOCITY
 	
 	#var clone_dir : Vector2 = get_global_mouse_position() - global_position
@@ -139,6 +152,9 @@ func _physics_process(delta: float) -> void:
 func is_not_input_restricted():
 	return !is_in_knockback and !should_pause
 
+func can_jump():
+	return grounded or !able_to_jump_timer.is_stopped()
+	
 func boost_up():
 	play_random_hit_sound(enemy_hit_sound_array)
 	$Hit_effect/CPUParticles2D.emitting = true
