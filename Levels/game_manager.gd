3c02880574bfbbcf12398bfa7a1f6ad01305b830
dantal1602 <danieltal1602@gmail.com>
2025-08-24 22:35:25 +0300

    final level

--- Levels/game_manager.gd
+++ Levels/game_manager.gd
@@ -13,6 +13,7 @@ var level_index = 0
 
 func _ready():
 	menu.visible = false
+	levels.append(preload("res://Levels/final_level.tscn"))
 	levels.append(preload("res://Levels/first_level.tscn"))
 	levels.append(preload("res://Levels/second_level.tscn"))
 	levels.append(preload("res://Levels/third_level.tscn"))
